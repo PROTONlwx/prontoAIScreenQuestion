32e39d83d7043d204d68f271eda9289debf7d6f6
wenxuan liu <30834783+PROTONlwx@users.noreply.github.com>
2023-01-14 22:11:47 -0800

    finish

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void printGitStatus(String dir) {
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("bash", "-c", "cd " + dir);
-        processBuilder.command("bash", "-c", "git branch --show-current && git show -s --format=%ct && git log -1 | grep Author && git diff");
+        processBuilder.command("bash", "-c", "git branch --show-current && git show -s --format=%ct && git log -1 | grep Author && git status");
 
         try {
 
@@ -32,7 +32,7 @@ public class Main {
             if (exitVal == 0) {
                 String[] lines = output.toString().split("\n");
                 System.out.println("active branch: " + lines[0]);
-                System.out.println("local changes: " + (output.toString().indexOf("no change") > -1));
+                System.out.println("local changes: " + (!output.toString().contains("Changes not staged for commit:")));
                 System.out.println("recent commit: " + (System.currentTimeMillis() / 1000L - Integer.parseInt(lines[1]) < msInWeek));
                 System.out.println("blame Rufus: " + lines[2].substring(8, 13).equals("Rufus"));
                 System.exit(0);
